d6075dc8ebe5c5a5aaba44309c7762e84370d928
arnettryan <73567190+arnettryan@users.noreply.github.com>
2021-02-09 14:34:45 -0500

    nasa call added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-// adding css to jsx is that easy
-import './App.css'; // This pattern is preferred where css for this component has a matching .css filename
+import { useEffect, useState } from "react";
 
-// A component import
-import Navbar from './components/Navbar'
+import Nasa from './components/Nasa'
+// import Weather from './components/Weather'
+// import Zomato from './components/Resturant'
 
+const App = () => {
 
-// Defining our <App /> component the function name matches the file name
-function App() {
-  // All functional components need to return jsx with one parent element
-  return ( 
-    <div className="App"> {/* Parent Element. Also we can't use the word class, so we use className in jsx*/}
-      {/* Navbar is our imported component*/}
-      <Navbar />
+  const [location, setLocation] = useState({latitude: 0, longitude: 0})
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((pos)=> {
+      
+      console.log(pos)
+      setLocation(
+        {
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude
+        }  
+      )
+    });
+  },[])
+
+  return (
+    <div className="App">
+      {location.latitude}
+      {location.longitude}
+      <Nasa long={location.longitude} lat={location.latitude}/>
+      {/* <Weather long={location.longitude} lat={location.latitude}/>
+      <Zomato long={location.longitude} lat={location.latitude}/> */}
     </div>
-  );
+  )
 }
 
-// Makes our Component available for import
 export default App;
